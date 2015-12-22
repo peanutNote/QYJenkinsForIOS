fc17f9a3020da045b5a41cdf0777a1280d326c98
qianye <422794901@qq.com>
2015-12-22 19:09:57 +0800

    Initial Commit

---
+++ QYJenkinsForIOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QYJenkinsForIOS
+//
+//  Created by qianye on 15/12/22.
+//  Copyright © 2015年 qianye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QYJenkinsForIOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QYJenkinsForIOS
+//
+//  Created by qianye on 15/12/22.
+//  Copyright © 2015年 qianye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
